1f45631adca588800b53539f6fbfce4b5a913a26
kate <18810809501@163.com>
2019-07-16 16:12:41 +0800

    piyao的爬虫基本完成

--- spider_dynamic_piyao_sina.py
+++ spider_dynamic_piyao_sina.py
@@ -11,6 +11,7 @@ import platform
 from lxml import etree
 from datetime import datetime
 
+item =[]
 chrome_driver_path = ""
 main_page_url ="http://piyao.sina.cn/"
 if platform.system()=='Windows':
@@ -31,31 +32,37 @@ driver = webdriver.Chrome(chrome_options=chrome_options, \
 driver.get(main_page_url)
 time.sleep(5)
 
-item=[]
-for i in range(1,10):
-    titles=[]
-    numbers=[] 
+#逐渐滚动浏览器窗口，令ajax逐渐加载
+for i in range(0,100):
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    i += 1
+    time.sleep(4)
+
+number_s=[]
+title_s=[]
+for i in range(1,100):
+
     time =  driver.find_element_by_xpath('//div[@class="zy_day" and position()='+str(i)+']/div[@class="day_date"]')
-    print(time.text)
-    titles.append(driver.find_element_by_xpath('//div[@class="zy_day" and position()='+str(i)+']/div[@class="day_date"]/following-sibling::ul//div[@class="left_title"]')
-    numbers.append(driver.find_elements_by_xpath('//div[@class="zy_day" and position()='+str(i)+']/div[@class="day_date"]/following-sibling::ul//div[@class="left_like"]')
-    number =[]
-    for i in numbers:
-        number.append(i.text())
-    z = zip(titles,numbers)
+    time = time.text
+    print(time)
+    titles=driver.find_elements_by_xpath('//div[@class="zy_day" and position()='+str(i)+']/div[@class="day_date"]/following-sibling::ul//div[@class="left_title"]')
+    numbers=driver.find_elements_by_xpath('//div[@class="zy_day" and position()='+str(i)+']/div[@class="day_date"]/following-sibling::ul//div[@class="like_text"]')
+    for i in titles:
+        title_s.append(i.text)
+    for j in numbers:
+        number_s.append(j.text)
+
+    z = zip(title_s,number_s)
     for title ,number in z:
         dic={}
         dic['title'] = title
         dic['time'] = time
-        dic['number'] = number
+        dic['number'] = int(number)
         print(number)
-
         item.append(dic)
 
-for i in item:
-    i.number=int(i.number)
-
-
-
-
-
+driver.quit()
+sorted(item,key=lambda x:x['number'],reverse=False)
+print("谣言排名前10的依次是：",end="\n")
+for i in item[:10]:
+    print("谣言：",'\t',i['title'],'\t',"点赞数：",'\t',i['number'],'\t',"时间：",'\t',i['time'],end='\n')
